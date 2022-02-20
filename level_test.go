d8b8c4d8a8e37e8fbe710bc259881091b4b91bad
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-02-20 14:02:30 +0700

    feat(logging): Add level_test

--- level_test.go
+++ level_test.go
@@ -1,1 +1,18 @@
 package GolangLogging
+
+import (
+	"github.com/sirupsen/logrus"
+	"testing"
+)
+
+func TestLevel(t *testing.T) {
+	logger := logrus.New()
+	logger.SetLevel(logrus.TraceLevel)
+
+	logger.Trace("This is trace")
+	logger.Debug("This is Debug")
+	logger.Info("This is Info")
+	logger.Warn("This is Warning")
+	logger.Error("This is Error")
+
+}
