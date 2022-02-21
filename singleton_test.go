e25ee879dd6717bc10c7b8396cfbb0a19bf2a41a
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-02-21 10:05:25 +0700

    feat(logging): Add singleton_test

---
+++ singleton_test.go
@@ -0,0 +1,17 @@
+package GolangLogging
+
+import (
+	"github.com/sirupsen/logrus"
+	"testing"
+)
+
+func TestSingleton(t *testing.T) {
+	logrus.Info("Hello World")
+	logrus.Warn("Hello World")
+
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+
+	logrus.Info("Hello World")
+	logrus.Warn("Hello World")
+
+}
