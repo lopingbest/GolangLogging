3b5f1c249a1e039de52d4f89025137a4e6b2d91c
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-02-21 09:05:14 +0700

    feat(field_test): Add TestFIeld

---
+++ field_test.go
@@ -0,0 +1,17 @@
+package GolangLogging
+
+import (
+	"github.com/sirupsen/logrus"
+	"testing"
+)
+
+func TestField(t *testing.T) {
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.JSONFormatter{})
+
+	logger.WithField("username", "galih").Info("Hello World")
+
+	logger.WithField("username", "setiadi").
+		WithField("name", "Galih Setiadi").
+		Info("Hello World")
+}
